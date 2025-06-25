163154fe7d776cca89b9fc7464cb13bfa4020fb8
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-06-25 15:25:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO Application
+
+A modern TODO application using Typescript, Javascript, HTML, CSS, and the Hono framework. Built with TDD methodology.
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+# Setup script for TODO Application
+
+# Install dependencies
+echo "Installing dependencies..."
+
+# Add your setup commands here
